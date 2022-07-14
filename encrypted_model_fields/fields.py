e5dd5976ab840629b4ab34992ec613e2ff38a88a
Mario Orlandi <morlandi@brainstorm.it>
2022-07-14 19:40:35 +0200

    First working implementation of EncryptedJSONField

--- encrypted_model_fields/fields.py
+++ encrypted_model_fields/fields.py
@@ -184,123 +184,7 @@ class EncryptedBigIntegerField(EncryptedNumberMixin, django.db.models.BigInteger
     pass
 
 #################################################################################
-
-
-def trace_method(fn):
-    """
-    Sample usage:
-
-        class MyClass(object):
-            ...
-
-            @trace_method
-            def myfunc(self, user, obj):
-                ...
-    """
-    def trace1(message):
-        print('\x1b[1;37;44m %s \x1b[1;39;49m' % str(message))
-
-    def trace2(message):
-        print('\x1b[1;33;40m %s \x1b[1;39;49m' % str(message))
-
-    def trace3(message):
-        print('\x1b[0;36;40m %s \x1b[1;39;49m' % str(message))
-
-    def func_wrapper(*args, **kwargs):
-
-        trace1('>>> %s()' % fn.__name__)
-
-        # remove "self"
-        targs = args[1:]
-        if (targs):
-            for arg in targs:
-                trace2(arg)
-
-        if kwargs:
-            for key, value in kwargs.items():
-                trace2('"%s": %s' % (key, value))
-
-        ret = fn(*args, **kwargs)
-        trace3(ret)
-        trace1('<<< %s()' % fn.__name__)
-        return ret
-    return func_wrapper
-
-
-# https://medium.com/@pedro.mvsilva/how-to-encrypt-the-values-of-a-postgres-jsonfield-in-django-abd2d9e802bf
-
-
-def encrypt_values(data, skip_keys=None):
-    # cypher = AES.new(
-    #     FIELD_ENCRYPTION_KEY, AES.MODE_CBC, FIELD_ENCRYPTION_IV
-    # )
-
-    if skip_keys is None:
-        skip_keys = []
-
-    if isinstance(data, (list, tuple, set)):
-        return [encrypt_values(x, skip_keys) for x in data]
-
-    if isinstance(data, dict):
-        return {
-            key: encrypt_values(value, skip_keys).decode()
-            for key, value in data.items()
-        }
-
-    return encrypt_str(repr(data))
-
-
-
-
-    # if isinstance(data, int) or isinstance(data, float):
-    #     #data = str(data).encode()
-    #     return encrypt_str(str(data))
-    #     #return b64encode(cypher.encrypt(pad(data, AES.block_size))).decode()
-
-    # if data is None:
-    #     #data = "".encode()
-    #     return encrypt_str("")
-    #     #return b64encode(cypher.encrypt(pad(data, AES.block_size))).decode()
-
-    # if isinstance(data, str):
-    #     #data = data.encode()
-
-    #     return encrypt_str(data)
-    #     #return b64encode(cypher.encrypt(pad(data, AES.block_size))).decode()
-
-    return json.dumps(data, cls=DjangoJSONEncoder)
-
-
-def decrypt_values(data):
-    # d_cypher = AES.new(
-    #     settings.FIELD_ENCRYPTION_KEY, AES.MODE_CBC, settings.FIELD_ENCRYPTION_IV
-    # )
-
-    if isinstance(data, (list, tuple, set)):
-        return [decrypt_values(x) for x in data]
-
-    if isinstance(data, dict):
-        return {key: decrypt_values(value) for key, value in data.items()}
-
-    if isinstance(data, str):
-        #data = b64decode(data)
-        data = decrypt_str(data)
-
-    return data
-
-    # try:
-    #     #value = unpad(d_cypher.decrypt(data), AES.block_size)
-    #     value = decrypt_str(data)
-    # except ValueError:
-    #     value = data
-
-    # value = value.decode()
-    # try:
-    #     return json.loads(value)
-    # except Exception:
-    #     value = check_for_bool(value)
-    #     return value
-
+# Encryption for JSONField
 
 class EncryptedJSONField(django.db.models.JSONField):
 
@@ -308,151 +192,86 @@ class EncryptedJSONField(django.db.models.JSONField):
         self.skip_keys = kwargs.pop("skip_keys", [])
         super().__init__(*args, **kwargs)
 
-    # # def from_db_value(self, value, expression, context):
-    # #     value = decrypt_values(value)
-    # #     return value
+    def get_prep_value(self, value):
+        """
+        Adapted from django.db.models.JSONField to apply encryption;
+        - all simple items will be encrypted;
+        - any dictionary will have all the values encrypted, while the keys will remain untouched.
+        The resulting value can be safely JSON-serialized
+        """
+        if value is None:
+            return value
+        # encrypt the values before serialization
+        value = encrypt_values(value)
+        return json.dumps(value, cls=self.encoder)
 
-    # # def get_prep_value(self, value):
-    # #     if self.blank and value == "":
-    # #         if self.default != NOT_PROVIDED:
-    # #             if callable(self.default):
-    # #                 value = self.default()
-    # #             else:
-    # #                 value = self.default
-    # #         else:
-    # #             value = {}
-
-    # #     if self.null and value is None:
-    # #         return None
-
-    # #     value = encrypt_values(value)
-    # #     import ipdb; ipdb.set_trace()
-
-
-    # # def to_python(self, value):
-    # #     if value is None:
-    # #         return value
-
-    # #     # if isinstance(value, (bytes, str)):
-    # #     #     if isinstance(value, bytes):
-    # #     #         value = value.decode('utf-8')
-    # #     #     try:
-    # #     #         value = decrypt_str(value)
-    # #     #     except cryptography.fernet.InvalidToken:
-    # #     #         pass
-
-    # #     return super().to_python(value)
-
-
-    # def to_python(self, value):
-    #     if value is None:
-    #         return value
-
-    #     #value = decrypt_values(value)
-    #     # if isinstance(value, (bytes, str)):
-    #     #     if isinstance(value, bytes):
-    #     #         value = value.decode('utf-8')
-    #     #     try:
-    #     #         value = decrypt_values(value)
-    #     #     except cryptography.fernet.InvalidToken:
-    #     #         pass
-
-    #     import ipdb; ipdb.set_trace()
-    #     value = json.loads(value)
-    #     return super().to_python(value)
-
-    # def from_db_value(self, value, *args, **kwargs):
-    #     value = super().from_db_value(value, *args, **kwargs)
-    #     return self.to_python(value)
-
-    # # def get_prep_value(self, value):
-    # #     import ipdb; ipdb.set_trace()
-    # #     if value is None:
-    # #         return value
-    # #     return json.dumps(value, cls=self.encoder)
-
-    # def get_db_prep_save(self, value, connection):
-    #     #value = encrypt_values(value)
-    #     value = super().get_db_prep_save(value, connection)
-    #     if value is None:
-    #         return value
-    #     return json.dumps(value, cls=DjangoJSONEncoder)
-
-
-
-    #     # value = self.get_prep_value(value)
-
-    #     # if value is None:
-    #     #     return value
-    #     # # decode the encrypted value to a unicode string, else this breaks in pgsql
-    #     # #return (encrypt_str(str(value))).decode('utf-8')
-
-    #     # import ipdb; ipdb.set_trace()
-
-    #     #return value
-
-    @trace_method
     def from_db_value(self, value, expression, connection):
+        """
+        Adapted from django.db.models.JSONField to descrypt the values
+        """
+        from django.db.models.fields.json import KeyTransform
+
         if value is None:
             return value
         # Some backends (SQLite at least) extract non-string values in their
         # SQL datatypes.
-        from django.db.models.fields.json import KeyTransform
-
         if isinstance(expression, KeyTransform) and not isinstance(value, str):
             return value
         try:
-            return json.loads(value, cls=self.decoder)
+            #return json.loads(value, cls=self.decoder)
+            # Deserialize the JSON,
+            # then decrypt the values of the resulting object
+            obj = json.loads(value, cls=self.decoder)
+            return decrypt_values(obj)
         except json.JSONDecodeError:
             return value
 
-    @trace_method
-    def get_prep_value(self, value):
-        if value is None:
-            return value
-        return json.dumps(value, cls=self.encoder)
 
-    # def get_transform(self, name):
-    #     import ipdb; ipdb.set_trace()
-    #     transform = super().get_transform(name)
-    #     if transform:
-    #         return transform
-    #     return KeyTransformFactory(name)
+def encrypt_values(data, skip_keys=None):
+    # Inspired by:
+    #   - Pedro Silva: "How to encrypt the values of a Postgres JSONField in Django"
+    #     https://medium.com/@pedro.mvsilva/how-to-encrypt-the-values-of-a-postgres-jsonfield-in-django-abd2d9e802bf
+    #   - LucasRoesler: "django-encrypted-json"
+    #     https://github.com/LucasRoesler/django-encrypted-json
+    if skip_keys is None:
+        skip_keys = []
 
-    @trace_method
-    def validate(self, value, model_instance):
-        super().validate(value, model_instance)
-        try:
-            json.dumps(value, cls=self.encoder)
-        except TypeError:
-            raise exceptions.ValidationError(
-                self.error_messages['invalid'],
-                code='invalid',
-                params={'value': value},
-            )
+    # Scan the lists, then encode each item recursively
+    if isinstance(data, (list, tuple, set)):
+        return [encrypt_values(x, skip_keys) for x in data]
 
-    # def value_to_string(self, obj):
-    #     import ipdb; ipdb.set_trace()
-    #     return self.value_from_object(obj)
+    # Scan the dicts, then encode each item recursively
+    if isinstance(data, dict):
+        return {
+            key: encrypt_values(value, skip_keys)
+            for key, value in data.items()
+        }
 
+    # We finally have a simple item to work with, which can be:
+    # a string, a number, a boolean, or null.
+    # Since we don't want lo lose the item's type, we apply repr()
+    # to obtain a printable representational string of it,
+    # before proceding with the encryption
+    encrypted_data = encrypt_str(repr(data))
 
-
-
+    # Return the result as string, so that it can be JSON-serialized later on
+    return encrypted_data.decode()
 
 
+def decrypt_values(data):
 
+    # Scan the lists, then decode each item recursively
+    if isinstance(data, (list, tuple, set)):
+        return [decrypt_values(x) for x in data]
 
-# >>> pickle.dumps(1)
-# b'\x80\x04K\x01.'
-# >>> pickle.loads(pickle.dumps(1))
-# 1
-# >>> pickle.loads(pickle.dumps(1.3))
-# 1.3
-# >>> type(pickle.loads(pickle.dumps(1.3)))
-# <class 'float'>
-# >>> type(pickle.loads(pickle.dumps(1)))
-# <class 'int'>
-# >>> type(pickle.loads(pickle.dumps('abc')))
-# <class 'str'>
-# >>> type(pickle.loads(pickle.dumps(True)))
-# <class 'bool'>
+    # Scan the dicts, then decode each item recursively
+    if isinstance(data, dict):
+        return {key: decrypt_values(value) for key, value in data.items()}
+
+    # If we got so far, the data must be a string (the encrypted value)
+    data = decrypt_str(data)
+
+    # for many Python types, when the result from repr() is passed to eval()
+    # we will get the original object;
+    # we take advantage of this to reconstruct both the original value and type
+    return eval(data)
